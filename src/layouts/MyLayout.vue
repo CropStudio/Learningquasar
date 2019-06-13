f0419c742eeb7e850518889beab5cc17396c6992
FrdFaridaa <frdfarida29@gmail.com>
2019-06-13 12:16:05 +0700

    first commit

---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,85 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+        >
+          <q-icon name="menu" />
+        </q-btn>
+
+        <q-toolbar-title>
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      bordered
+      content-class="bg-grey-2"
+    >
+      <q-list>
+        <q-toolbar class="text-white bg-primary shadow-5">
+          <q-toolbar-title>
+            My Website
+          </q-toolbar-title>
+        </q-toolbar>
+        <div class="q-pa-md">
+              <q-list padding class="menu-list">
+                <q-item active clickable v-ripple to="/">
+                  <q-item-section avatar>
+                    <q-icon name="inbox" />
+                  </q-item-section>
+
+                  <q-item-section>
+                    Biodata
+                  </q-item-section>
+                </q-item>
+
+                <q-item clickable v-ripple to="/inputbiodata">
+                  <q-item-section avatar>
+                    <q-icon name="star" />
+                  </q-item-section>
+
+                  <q-item-section>
+                    Input Biodata
+                  </q-item-section>
+                </q-item>
+              </q-list>
+        </div>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { openURL } from 'quasar'
+
+export default {
+  name: 'MyLayout',
+  data () {
+    return {
+      leftDrawerOpen: this.$q.platform.is.desktop,
+      drawer: true
+    }
+  },
+  methods: {
+    openURL
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .menu-list .q-item
+    border-radius 0 32px 32px 0
+</style>
---
+++ src/pages/biodata/input_biodata.vue
@@ -0,0 +1,112 @@
+<template>
+    <div class="q-pa-md" style="max-width: 400px">
+
+        <q-form
+                @submit="onSubmit"
+                @reset="onReset"
+                class="q-gutter-md"
+        >
+            <q-input
+                    filled
+                    v-model="npm"
+                    label="NPM"
+                    hint="NPM"
+                    lazy-rules
+                    :rules="[ val => val && val.length > 0 || 'Please type your NPM']"
+            />
+            <q-input
+                    filled
+                    v-model="nama"
+                    label="NAMA"
+                    hint="NAMA"
+            />
+            <q-input
+                    filled
+                    v-model="prodi"
+                    label="PRODI"
+                    hint="PRODI"
+            />
+            <q-input
+                    filled
+                    v-model="ttl"
+                    label="TTL"
+                    hint="Tempat Tanggal Lahir"
+            />
+            <q-input
+                    filled
+                    v-model="alamat"
+                    label="Alamat"
+                    hint="Alamat"
+            />
+            <q-input
+                    filled
+                    v-model="email"
+                    label="Email"
+                    hint="Email"
+            />
+            <q-input
+                    filled
+                    v-model="nohp"
+                    label="No Handphone"
+                    hint="No Handphone"
+            />
+            <q-input
+                    filled
+                    v-model="nohportu"
+                    label="No Handphone Orang Tua"
+                    hint="No Handphone Orang Tua"
+            />
+
+            <q-toggle v-model="accept" label="I accept the license and terms" />
+
+            <div>
+                <q-btn label="Submit" type="submit" color="primary"/>
+                <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+            </div>
+        </q-form>
+
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: null,
+      age: null,
+
+      accept: false
+    }
+  },
+
+  methods: {
+    onSubmit () {
+      if (this.accept !== true) {
+        this.$q.notify({
+          color: 'red-5',
+          textColor: 'white',
+          icon: 'fas fa-exclamation-triangle',
+          message: 'You need to accept the license and terms first'
+        })
+      } else {
+        this.$q.notify({
+          color: 'green-4',
+          textColor: 'white',
+          icon: 'fas fa-check-circle',
+          message: 'Submitted'
+        })
+      }
+    },
+
+    onReset () {
+      this.name = null
+      this.age = null
+      this.accept = false
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
